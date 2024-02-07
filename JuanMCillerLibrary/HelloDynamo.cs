79a9cd54f11bfa17552a9fe99a71a693bd45ea17
Juan Martínez <juan.m.ciller@gmail.com>
2024-02-07 18:16:08 +0100

    Dynamo environment
    Project setup completed #3

---
+++ JuanMCillerLibrary/HelloDynamo.cs
@@ -0,0 +1,18 @@
+﻿namespace JuanMCillerLibrary
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class HelloDynamo
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string SayHello(string name)
+        {
+            return "Hello " + name + "!";
+        }
+    }
+}
