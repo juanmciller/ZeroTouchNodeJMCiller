2f7f3f6c8586face1e16de35acbd7c37665cc86c
Juan Martínez <juan.m.ciller@gmail.com>
2024-02-12 19:40:43 +0100

    Update
    Strings and gettype added

---
+++ JuanMCillerLibrary/CsharpBasics/MyFirstNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JuanMCillerLibrary
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MyFirstNode
+    {
+        private MyFirstNode()
+        {
+            
+        }
+        /// <summary>
+        /// This is our first method to retrieve the Data Type of an input.
+        /// </summary>
+        /// <param name="myVariable">This variable can be whatever input data type.</param>
+        /// <returns>A C# Data type</returns>
+        public static Type GetDataType(object myVariable)
+        {
+            return myVariable.GetType();
+        }
+    }
+}
---
+++ JuanMCillerLibrary/CsharpBasics/Strings.cs
@@ -0,0 +1,29 @@
+﻿namespace JuanMCillerLibrary
+{
+    public class Strings
+    {
+        private Strings()
+        {
+            
+        }
+        /// <summary>
+        /// This is a method to concatenate two strings.
+        /// </summary>
+        /// <param name="myString1">My first string input.</param>
+        /// <param name="myString2">My second string input.</param>
+        /// <returns>The result of merging two string inputs.</returns>
+        public static string ConcatenateStrings(string myString1, string myString2)
+        {
+            return myString1 + myString2;
+        }
+        /// <summary>
+        /// This code gets the length of a string.
+        /// </summary>
+        /// <param name="myString">String input.</param>
+        /// <returns>Returns an integer.</returns>
+        public static int GetStringLength(string myString)
+        {
+            return myString.Length;
+        }
+    }
+}
